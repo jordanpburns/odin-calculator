f7a8364be14a106f53e6c2820fe4985d12dda44c
Jordan Burns <jordanpburns13@gmail.com>
2024-05-15 10:09:05 -0700

    add html boilerplate and JS math functions

---
+++ calculator.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+        return add(firstNumber, secondNumber);
+
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        
+        case '*':
+            return multiply(firstNumber, secondNumber);
+
+        case '/':
+            return divide(firstNumber, secondNumber);
+
+    }
+}
+
+console.log(operate(5,5,'+'));
+console.log(operate(5,5,'-'));
+console.log(operate(5,5,'*'));
+console.log(operate(5,5,'/'));
