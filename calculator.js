3b4425258fc68d6c58ac76f6f718164879700d0c
Jordan Burns <jordanpburns13@gmail.com>
2024-05-15 17:56:43 -0700

    implement basic functionality

--- calculator.js
+++ calculator.js
@@ -14,10 +14,6 @@ function divide(a, b) {
     return a / b
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
-
 function operate(firstNumber, secondNumber, operator) {
     switch (operator) {
         case '+':
@@ -35,7 +31,83 @@ function operate(firstNumber, secondNumber, operator) {
     }
 }
 
-console.log(operate(5,5,'+'));
-console.log(operate(5,5,'-'));
-console.log(operate(5,5,'*'));
-console.log(operate(5,5,'/'));
+const numbersContainer = document.querySelector("#numbers-container");
+
+for (i=0; i<3; i=i+1) {
+    let row = document.createElement("div");
+    for (j=0; j<3; j=j+1) {
+        let button = document.createElement("button");
+        button.textContent = `${i*3 + j + 1}`;
+        row.appendChild(button);
+    }
+    numbersContainer.appendChild(row);
+}
+
+row = document.createElement("div");
+const lastRowSymbols = ["0"];
+
+lastRowSymbols.forEach((value) => {
+    let button = document.createElement("button");
+    button.textContent = `${value}`;
+    row.appendChild(button);
+})
+numbersContainer.appendChild(row);
+numbersContainer.addEventListener("click", (event) => {
+    numberPress(event);
+});
+
+const operatorsContainer = document.querySelector("#operators-container");
+const operatorSymbols = ["+", "-", "*", "/", "="];
+
+operatorSymbols.forEach((value) => {
+   let button = document.createElement("button");
+   button.textContent = `${value}`;
+   operatorsContainer.appendChild(button);
+})
+
+operatorsContainer.addEventListener("click", (event) => {
+    operatorPress(event);
+})
+
+let display = document.querySelector("#display")
+
+function numberPress(event) {
+    let target = event.target;
+    if (target.textContent.length > 1) {
+        return;
+    }
+    let displayText = display.textContent;
+    if (displayText === "0") {
+        displayText = "";
+    }
+    if (!reset) {
+        displayText = "";
+        reset = true;
+    }
+    displayText += target.textContent;
+    display.textContent = displayText;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+let reset;
+
+function operatorPress(event) {
+    let target = event.target;
+    if (target.textContent === "=") {
+        if (operator === undefined) {
+            return;
+        }
+        let secondNumber = parseFloat(display.textContent);
+        let result = operate(firstNumber, secondNumber, operator);
+        display.textContent = result;
+        secondNumber = undefined;
+        operator = undefined;
+        firstNumber = result;
+    } else {
+        firstNumber = parseFloat(display.textContent);
+        operator = target.textContent;
+        reset = false;
+    }
+}
